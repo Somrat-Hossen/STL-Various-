1fdffb8681a3c1d6b3fb37af668df1ed15c65873
Somrat-Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2017-12-30 19:20:37 +0600

    Create First.cpp

---
+++ First.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include<iomanip>
+#define PI acos(-1)
+int arr[10000];
+
+using namespace std;
+
+int check(int n)
+{
+
+}
+
+int main()
+{
+    int n,i,sum=0;
+    char str[1010];
+    cin>>n;
+    getchar();
+
+    for(i=0;i<n;i++)
+    {
+       cin>>str[i];
+    }
+    for(i=0;i<n;i++)
+    {
+      cout<<str[i]<<" ";
+    }
+    return 0;
+}
